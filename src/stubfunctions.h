19ddf883b946d95052cae11a9a70dd51fe6696f3
Kenz1e314 <115039840+Kenz1e314@users.noreply.github.com>
2026-04-19 16:48:23 -0700

    LH:  stubfunctions.h - remove computerMove() declaration

--- src/stubfunctions.h
+++ src/stubfunctions.h
@@ -11,7 +11,7 @@
 
 // gamestate.c
 int legalMove(MOVE *playerMove, PIECE *board[RANKS][FILES], int playerNum, MOVELIST *logMoveList);
-MOVE computerMove(PIECE *board[RANKS][FILES], int playerNum);
+// MOVE computerMove(PIECE *board[RANKS][FILES], int playerNum);
 int castling(MOVE *playerMove, PIECE *board[RANKS][FILES], MOVELIST *logMoveList);
 int enPassant(MOVE *playerMove, PIECE *board[RANKS][FILES], MOVELIST *logMoveList);
 MOVELIST *possibleMoves(POS currentPos, PIECE *board[RANKS][FILES], MOVELIST *logMoves, int playerNum);
