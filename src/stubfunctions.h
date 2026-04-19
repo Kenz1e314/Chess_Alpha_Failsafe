5f54a241a32bd52de2b33585f78c9e375e7da106
Kenz1e314 <115039840+Kenz1e314@users.noreply.github.com>
2026-04-19 16:58:54 -0700

    LH:  stubfunctions.h - inCheck(), checkMate(), staleMate()

--- src/stubfunctions.h
+++ src/stubfunctions.h
@@ -9,7 +9,7 @@
 #ifndef STUBFUNCTIONS_H
 #define STUBFUNCTIONS_H
 
-// gamestate.c
+// gamestate.c calls
 int legalMove(MOVE *playerMove, PIECE *board[RANKS][FILES], int playerNum, MOVELIST *logMoveList);
 // MOVE computerMove(PIECE *board[RANKS][FILES], int playerNum);
 int castling(MOVE *playerMove, PIECE *board[RANKS][FILES], MOVELIST *logMoveList);
@@ -17,6 +17,12 @@ int enPassant(MOVE *playerMove, PIECE *board[RANKS][FILES], MOVELIST *logMoveLis
 MOVELIST *possibleMoves(POS currentPos, PIECE *board[RANKS][FILES], MOVELIST *logMoves, int playerNum);
 void deleteMoveList(MOVELIST *piecePossibleMoves);
 int logMove(MOVE *m);
+
+
+// gamestate.c functions
+int inCheck(void);
+int staleMate(void);
+int checkMate(void);
 
 
 #endif
