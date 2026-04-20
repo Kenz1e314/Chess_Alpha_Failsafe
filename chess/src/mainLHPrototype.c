fc6ccd67de2aa780ad1a41373e42654590edac35
Kenz1e314 <115039840+Kenz1e314@users.noreply.github.com>
2026-04-19 22:12:27 -0700

    Update mainLHPrototype.c-player number and turn updates

--- chess/src/mainLHPrototype.c
+++ chess/src/mainLHPrototype.c
@@ -184,14 +184,14 @@ int main(void){
     
     // prompting player turn input 
     printf("NOTE:  Right now, player vs. computer only works if the user picks the white piece, due to how computer moves work\n");
-    printf("Please type in which set you (player 1) want to use \n(white or black) (default white):  ");
+    printf("Please type in which set you want to use \n(white or black) (default white).  This will determine your player number, as white goes first:  ");
     scanf(" %15s", playerColor);
     if(strcmp(playerColor, "white") == 0){
         playerNum = 0;
         printf("Player 1 is playing with the white set.  Player 2 is playing with the black set.\n");
     } else if(strcmp(playerColor, "black") == 0){
         playerNum = 1;
-        printf("Player 1 is playing with the black set.  Player 2 is playing with the white set.\n");
+        printf("Player 1 is playing with the white set.  Player 2 is playing with the black set.\n");
     } else{
         playerNum = 0;
         printf("Going to default.  \nPlayer 1 is playing with the white set.  Player 2 is playing with the black set.\n\n");
